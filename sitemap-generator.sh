7de2e97909b31efc8ee5d6e3d9cd834f25c7b61d
Pat O'Brien <pat@obrienphoto.net>
2019-02-15 19:06:39 -0500

    Make it a little more crontab friendly

--- sitemap-generator.sh
+++ sitemap-generator.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 red=`tput setaf 1`
 green=`tput setaf 2`
@@ -29,6 +29,14 @@ OUTPUT="sitemap.xml"
 FREQUENCY="weekly"
 DATE=`date +%Y-%m-%d`
 VERBOSE=true
+WGET=$(which wget)
+SORT=$(which sort)
+SED=$(which sed)
+AWK=$(which awk)
+CAT=$(which cat)
+TR=$(which tr)
+WC=$(which wc)
+RM=$(which rm)
 
 while true; do
     case "$1" in
@@ -45,8 +53,8 @@ if [ -z "$URL" ]; then
     die "Usage: $0 [OPTIONS] url. Try $0 --help for more informations."
 fi
 
-TMP_TXT_FILE=$OUTPUT.txt
-SED_LOG_FILE=$OUTPUT.sedlog.txt
+TMP_TXT_FILE=/tmp/sitemap-generator.txt
+SED_LOG_FILE=/tmp/sitemap-generator.sedlog.txt
 
 log "URL: $URL"
 log "Output: $OUTPUT"
@@ -55,17 +63,17 @@ log "Priority: Dynamic"
 echo ""
 
 log "Crawling $URL => $TMP_TXT_FILE ..."
-wget --spider --recursive -e robots=off --output-file=$TMP_TXT_FILE --no-verbose --reject=.jpg,.jpeg,.css,.js,.ico,.png $URL
+$WGET --spider --recursive -e robots=off --output-file=$TMP_TXT_FILE --no-verbose --reject=.jpg,.jpeg,.css,.js,.ico,.png $URL
 
 echo ""
 log "Cleaning urls ..."
 # This sanitizes the &. Undoing that for now
 #sed -n "s@.\+ URL:\([^ ]\+\) .\+@\1@p" $TMP_TXT_FILE | sed "s@&@\&amp;@" > $SED_LOG_FILE
 # Pull URLs from wget's output
-sed -n "s@.\+ URL:\([^ ]\+\) .\+@\1@p" $TMP_TXT_FILE > $SED_LOG_FILE
+$SED -n "s@.\+ URL:\([^ ]\+\) .\+@\1@p" $TMP_TXT_FILE > $SED_LOG_FILE
 
 log "Sorting and removing any duplicates ..."
-sort -u -o $SED_LOG_FILE $SED_LOG_FILE
+$SORT -u -o $SED_LOG_FILE $SED_LOG_FILE
 
 log "Generating $OUTPUT ..."
 # Header from www.xml-sitemaps.com
@@ -78,11 +86,11 @@ echo '            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">' >> 
 # Loop over lines in the sed log file for parsing - https://unix.stackexchange.com/a/7012/67790
 IFS=$'\r\n'     # make newlines the only separator
 set -f          # disable globbing
-for i in $(cat < "$SED_LOG_FILE"); do
-    BASENAME=$(echo "$i" | awk -F[/:] '{print $4}')
+for i in $($CAT < "$SED_LOG_FILE"); do
+    BASENAME=$(echo "$i" | $AWK -F[/:] '{print $4}')
     #DIR=$(echo "$i" | sed -r 's|.*/([^/]+)/?$|\1|') # This returns the last part of the URL only (typically the page), and not the category + page. 
     #PATH_COUNT=$(echo "$i" | tr '/' ' ' | wc -w) # This counts everything. http is 1, basename is 2, first path is 3, page is 4, etc. Priority isn't accurate if URL  is in a subfolder to begin with
-    PATH_COUNT=$(echo "$i" | sed "s#${URL}*##" | tr '/' ' ' | wc -w) # This removes the input URL, then counts the number of folders in the path. 
+    PATH_COUNT=$(echo "$i" | $SED "s#${URL}*##" | $TR '/' ' ' | $WC -w) # This removes the input URL, then counts the number of folders in the path. 
     
     # Add a priority element. Basename URL and immediate first level pages are priority 1. Further down the path line get priority 0.8
     # http://www.sitemaps.org/protocol.html#prioritydef
@@ -92,7 +100,7 @@ for i in $(cat < "$SED_LOG_FILE"); do
         PRIORITY=0.8
     fi
     
-cat >>$OUTPUT <<EOL
+$CAT >>$OUTPUT <<EOL
     <url>
         <loc>$i</loc>
         <lastmod>$DATE</lastmod>
@@ -105,10 +113,10 @@ done
 echo '</urlset>' >> $OUTPUT
 
 log "Cleaning temp files ..."
-rm -f $TMP_TXT_FILE  $SED_LOG_FILE 
+$RM -f $TMP_TXT_FILE  $SED_LOG_FILE
 
 log "Cleaning wget's temp folder ..."
-rm -rf $BASENAME
+$RM -rf $BASENAME
 
 echo ""
 log "Done => $OUTPUT"
